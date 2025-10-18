506083194e89adb1f94ec12eabfa1a9043de8996
Lautaro Ariño <Lautaro.arino@gmail.com>
2025-10-18 21:30:24 +0200

    Added Odin

--- Assets/Scripts/TestZounds.cs
+++ Assets/Scripts/TestZounds.cs
@@ -1,64 +1,22 @@
+using Sirenix.OdinInspector;
 using System.Collections;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.Audio;
 using Zounds;
 
 public class TestZounds : MonoBehaviour {
 
-    public TextAsset jsonProject;
+    [Button]
+    public void Init()
+    {
+        ZoundEngine.Initialize();
 
-    void Start() {
-        // Warning: only call LoadFromJSON in build,
-        // because calling it in editor will reset your unsaved data
-#if !UNITY_EDITOR
-        ZoundsProject.LoadFromJSON(jsonProject);
-#endif
-
-        Init();
     }
 
-    private async void Init() {
-        float startTime = Time.realtimeSinceStartup;
-
-        await ZoundEngine.InitializeAsync();
-        //ZoundEngine.Initialize();
-
-        Debug.Log("Zounds async load time: " + (Time.realtimeSinceStartup - startTime));
-
-        StartCoroutine(TestCoroutine());
+    [Button]
+  public void PlayZound(string zoundName) {
+        
+        
+        ZoundEngine.PlayZound(zoundName);
+        
     }
-
-    private IEnumerator TestCoroutine() {
-
-        for (int i = 0; i < 100; i++) {
-            ZoundEngine.PlayZound("Golem Lord");
-            yield return new WaitForSeconds(0.05f);
-        }
-
-        for (int i = 0; i < 100; i++) {
-            ZoundEngine.PlayZound("Purple Prisoner Grab 1");
-            yield return new WaitForSeconds(0.05f);
-        }
-
-        yield return null;
-
-        ZoundEngine.PlayZound("Jester Major");
-
-        yield return null;
-
-        for (int i = 0; i < 100; i++) {
-            ZoundEngine.PlayZound("Slime Bite Smol 1");
-            yield return new WaitForSeconds(0.05f);
-        }
-
-        yield return null;
-
-        for (int i = 0; i < 100; i++) {
-            ZoundEngine.PlayZound("Grumpy Woman");
-            yield return new WaitForSeconds(0.05f);
-        }
-    }
-
 }
